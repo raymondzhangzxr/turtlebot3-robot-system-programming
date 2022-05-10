32e82df988e091b2c14c0ba3964f99e427f7fa8d
Xucheng Ma <mxcheng@umich.edu>
2022-05-09 21:47:03 -0400

    update wf frontier exploration

--- rsp_wf_frontier_exploration/src/front_exp_server.cpp
+++ rsp_wf_frontier_exploration/src/front_exp_server.cpp
@@ -20,16 +20,21 @@ bool FrontierExplorer::getGoal(
     float origin_y = map.info.origin.position.y;
 
     // float min_dis = std::numeric_limits<float>::infinity();
-    float max_dis = 0.0;
     int grid_x = (current_x - origin_x) / resolution;
     int grid_y = (current_y - origin_y) / resolution;
     std::vector<std::vector<int> > frontiers =
         wfd(map, map.info.height, map.info.width,
             grid_x + (grid_y * map.info.width));
 
-
     res.goal_pose.x = current_x;
     res.goal_pose.y = current_y;
+
+    turtle_pose nearest_pose;
+    turtle_pose local_furthest_pose;
+    float local_radius = 1.0;
+
+    float local_max_dis = 0.0;
+    float min_dis = std::numeric_limits<float>::infinity();
 
     for (int i = 0; i < frontiers.size(); i++) {
         for (int j = 0; j < frontiers[i].size(); j++) {
@@ -40,24 +45,23 @@ bool FrontierExplorer::getGoal(
             float diff_x = x - current_x;
             float diff_y = y - current_y;
 
-            if(fabs(diff_x) > 2.5 || fabs(diff_y) > 2.5){
-                continue;
+            float dis = sqrt(diff_x * diff_x + diff_y * diff_y);
+            if (dis < min_dis) {
+                // return the closest frontier points
+                min_dis = dis;
+                nearest_pose.x = x;
+                nearest_pose.y = y;
             }
 
-            float dis = sqrt(diff_x * diff_x + diff_y * diff_y);
-            if (dis > max_dis) {
-                // return the closest frontier points
-                max_dis = dis;
-                res.goal_pose.x = x;
-                res.goal_pose.y = y;
+            if (dis < local_radius && dis > local_max_dis) {
+                local_max_dis = dis;
+                local_furthest_pose.x = x;
+                local_furthest_pose.y = y;
             }
         }
     }
 
-    // float x = ((frontiers[0][0] % map.info.width)) * resolution + origin_x;
-    // float y = ((frontiers[0][0] / map.info.width)) * resolution + origin_y;
-    // res.goal_pose.x = x;
-    // res.goal_pose.y = y;
+    res.goal_pose = min_dis < local_radius ? local_furthest_pose : nearest_pose;
 
     // no yaw change
     res.goal_pose.yaw = current_pose.yaw;
